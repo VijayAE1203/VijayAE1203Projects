e2889e81566a7701561299fc8d36042c3c3b994b
VijayAE1203 <147577863+VijayAE1203@users.noreply.github.com>
2024-06-22 19:13:02 -0500

    Delete UnitTesting/CPP/classify_obstacles/bazel-classify_obstacles directory

--- UnitTesting/CPP/classify_obstacles/bazel-classify_obstacles/external/local_config_cc/get_env.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\VCVARSALL.BAT" amd64  -vcvars_ver=14.37.32822 > NUL 
-echo PATH=%PATH%,INCLUDE=%INCLUDE%,LIB=%LIB%,WINDOWSSDKDIR=%WINDOWSSDKDIR% 
